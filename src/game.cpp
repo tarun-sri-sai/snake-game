04526ce4da9a9554fa2b5e2d761c680b99f1566b
tarun-sri-sai <76744890+tarun-sri-sai@users.noreply.github.com>
2026-03-28 09:34:47 +0530

    Use generic fonts

--- src/game.cpp
+++ src/game.cpp
@@ -8,8 +8,8 @@ Game::Game() : m_context(std::make_shared<GameContext>())
 {
     m_context->window->create(sf::VideoMode({ 960, 544 }), "Snake Game", sf::Style::Close);
 
-    m_context->assets->addFont(TITLE_FONT, getAssetFile("knight_warrior.otf"));
-    m_context->assets->addFont(MENU_FONT, getAssetFile("supreme_spike.otf"));
+    m_context->assets->addFont(TITLE_FONT, getAssetFile("roboto_bold.ttf"));
+    m_context->assets->addFont(MENU_FONT, getAssetFile("roboto_regular.ttf"));
 
     m_context->assets->addTexture(GRASS, getAssetFile("grass.png"), true);
     m_context->assets->addTexture(WALL, getAssetFile("wall.png"), true);
